05efed6a3f26f26d251a20d227230c32b201ad8b
sentator <sentator.dev@gmail.com>
2024-05-07 17:26:02 +0300

    feat: add dotenv

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,15 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
 const { Pool } = pg;
 
+dotenv.config();
+
 const pool = new Pool({
-  host: 'postgres',
-  user: 'postgres',
-  port: 5432,
-  password: 'L1953TR',
-  database: 'users_express',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  user: process.env.POSTGRES_USER,
+  port: Number(process.env.POSTGRES_PORT),
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
 });
 
 export default pool;
