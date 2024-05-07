3bca3c05691675ea258b8453a37222e56938dd94
sentator <98660576+sentator@users.noreply.github.com>
2024-05-07 18:02:08 +0300

    feat: implement postgres database for users and docker-compose setup
    * feat: implement postgres database for users
    * feat: add dockerfile and docker-compose config
    * feat: add dotenv
    * chore: add env variables and dedicated database folder
    * chore: add port env for postgres service

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 import usersRouter from './routes/users.routes';
 import { cors } from './middlewares';
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(cors);
 
 app.use('/users', usersRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.`);
