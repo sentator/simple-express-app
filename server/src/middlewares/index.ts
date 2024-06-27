3145f4b3f9c6eed7f1a936d6c8c251269369943d
sentator <sentator.dev@gmail.com>
2024-06-27 20:52:54 +0300

    feat: add payload validation for /login and /registration endpoints

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -2,5 +2,6 @@ import cors from './cors';
 import checkValidId from './checkValidId';
 import errorMiddleware from './error';
 import authMiddleware from './auth';
+import validateBody from './validateBody';
 
-export { cors, checkValidId, errorMiddleware, authMiddleware };
+export { cors, checkValidId, errorMiddleware, authMiddleware, validateBody };
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { user } from '../controller';
+import { validateBody } from '../middlewares';
+import { registrationBodySchema, loginBodySchema } from '../schemas/user';
 
 const router = Router();
 
-router.post('/registration', user.registration);
-router.post('/login', user.login);
+router.post(
+  '/registration',
+  validateBody(registrationBodySchema),
+  user.registration,
+);
+router.post('/login', validateBody(loginBodySchema), user.login);
 router.post('/logout', user.logout);
 router.get('/refresh', user.refresh);
 
