07437c31ba8969e0da2f1006faba9f90401f8a54
sentator <sentator.dev@gmail.com>
2024-07-06 16:06:16 +0300

    chore: add validation for /messages endpoint

--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import { message } from '../controller';
+import { checkValidId, validateBody } from '../middlewares';
+import { createMessageBodySchema } from '../schemas/message';
 
 const router = Router();
 
 router.get('/', message.getAll);
-router.post('/', message.create);
-router.put('/:id', message.update);
-router.delete('/:id', message.delete);
+router.post('/', validateBody(createMessageBodySchema), message.create);
+router.put(
+  '/:id',
+  checkValidId,
+  validateBody(createMessageBodySchema),
+  message.update,
+);
+router.delete('/:id', checkValidId, message.delete);
 
 export default router;
